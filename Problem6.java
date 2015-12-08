039ed1dd8be782cfa3bd1d8a43dcfcb978513a4c
Sean Titus <stitus7@gatech.edu>
2015-12-08 04:56:47 -0500

    Problem 6

---
+++ Problem6.java
@@ -0,0 +1,7 @@
+public class Problem6 {
+    public static void main(String[] args) {
+        PrimeGen pg = new PrimeGen(1000000);
+        System.out.println(pg.getPrimesList().get(10000));
+    }
+}
+        
